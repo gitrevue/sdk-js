a54a53002857403eaab9e645ac604dc7b71a7016
Wade Urry <wade@iwader.co.uk>
2020-02-20 08:49:25 +0000

    This type is now in the definitions file

--- src/Api/AbstractApi.ts
+++ src/Api/AbstractApi.ts
@@ -1,24 +1,5 @@
 import Http from "../Http";
 
-export interface ResourceCollection<Entity> {
-  data: Entity[];
-  links: {
-    first: string;
-    last: string;
-    prev: string | null;
-    next: string | null;
-  };
-  meta: {
-    current_page: number;
-    from: number;
-    last_page: number;
-    path: string;
-    per_page: number;
-    to: number;
-    total: number;
-  };
-}
-
 abstract class AbstractApi {
   protected readonly http: Http;
 
